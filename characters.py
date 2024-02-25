868e1c78bdd50696140029b6e4dcc9f4beb5760b
zcsager <161096556+zcsager@users.noreply.github.com>
2024-02-25 11:05:19 -0600

    feat: after killing dinosaurs, switch to play as dinosaur

--- characters.py
+++ characters.py
@@ -17,6 +17,31 @@ class Explosion(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
 
+class PlayerDinosaur(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./dino.PNG")
+        self.rect = self.image.get_rect()
+        self.rect.center=(360,720)  #spawn in the middle
+        self.rect.bottom=(720)
+        
+
+    def update(self):
+
+        pressed_keys = pygame.key.get_pressed()
+        #print(self.rect.collidepoint(pygame.mouse.get_pos()));
+        
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-3,0)
+        if self.rect.right < 1280:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(3,0)
+
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
 class Dinosaur(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -59,6 +84,51 @@ class Dinosaur(pygame.sprite.Sprite):
         #only draw if dinosaur is still alive
         surface.blit(self.image, self.rect)
 
+
+class EnemyMeteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./met1.PNG")
+        self.rect = self.image.get_rect()
+        self.rect.center=(600,200)
+        self.move_right = True
+    
+    def move(self):
+
+        
+        self.rect.move_ip(0, 5)
+        #if dinosaur hits the bounds of the window
+        if (self.rect.right > 1280 or self.rect.left < 0):
+            #invert movement and place the dinosaur back in bounds
+            if(self.move_right == True):    
+                self.move_right = False
+                self.rect.right = 1280
+            else:
+                self.move_right = True
+                self.rect.left = 0
+
+        #if dinosaur is within bounds
+        else:
+            #move according to previously set direction
+            if (self.move_right == True):
+                self.rect.move_ip(5, 0)
+            else:
+                self.rect.move_ip(-5, 0)
+
+            #randomly change movement direction
+            if (random.randint(0, 50) == 0):
+                if (self.move_right == True):
+                    self.move_right = False
+                else:
+                    self.move_right = True
+
+
+        if self.rect.bottom > 720:
+            self.rect.center=(640, -300)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+            
 
 class Meteor(pygame.sprite.Sprite):
     def __init__(self):
--- game.py
+++ game.py
@@ -28,6 +28,8 @@ explosion_x = 0
 explosion_y = 0
 FONT = pygame.font.SysFont("freesansbold", 100)
 explode = False
+stage = 1
+score = False
 
 #explosion = pygame.sprite.Sprite()
 #explosion.rect = pygame.image.get_rect(explosion.image)
@@ -40,77 +42,170 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
+    #stage change when all dinosaurs killed
+    if len(enemys) == 0:
+        stage = 2
+        enemys = []
+        for i in range(7):
+            enemys.append(EnemyMeteor())
+        player = PlayerDinosaur()
 
-    #screen.fill("red")
-    screen.blit(bg,(0, 0))
-    text = FONT.render(str(num), True, white)
-    textRect =  text.get_rect()
-    textRect.center = (400//2, 400//2)
-    screen.blit(text, textRect)
+
+    #PLAY AS METEOR
+    if stage == 1:
+
+        # fill the screen with a color to wipe away anything from last frame
+
+        #screen.fill("red")
+        screen.blit(bg,(0, 0))
+        text = FONT.render(str(num), True, white)
+        textRect =  text.get_rect()
+        textRect.center = (400//2, 400//2)
+        screen.blit(text, textRect)
 
 
-    # RENDER GAME HERE
-    for i in enemys:
-        
-        if player.rect.colliderect(i.rect):
-            #player.image = pygame.image.load("./met1.PNG")
-            #player.image.get_rect()
-            explosion_x = player.rect.x
-            explosion_y = player.rect.y
-            player.rect.center=(360, 0)
-            expl.rect.center=(explosion_x, 720);
-            print("Hit")
+        # RENDER GAME HERE
+        for i in enemys:
+            
+            if player.rect.colliderect(i.rect):
+                #player.image = pygame.image.load("./met1.PNG")
+                #player.image.get_rect()
+                explosion_x = player.rect.x
+                explosion_y = player.rect.y
+                player.rect.center=(360, 0)
+                expl.rect.center=(explosion_x, 720);
+                print("Hit")
+                explode = True
+                #freeze = 60 
+
+                #num += 1
+
+                #print(num)
+
+
+        if player.rect.bottom >= 720:
+            playerPosX = player.rect.x
+            playerPosY = player.rect.y
+
+        #draw explosion object when hitting the ground
+        if player.rect.collidepoint(playerPosX, playerPosY):
+            expl.drawGround(screen, playerPosX)
+            #freeze = 60
             explode = True
-            #freeze = 60 
+            print("ground collision")
 
-            #num += 1
-
-            #print(num)
-
-
-    if player.rect.bottom >= 720:
-        playerPosX = player.rect.x
-        playerPosY = player.rect.y
-
-    #draw explosion object when hitting the ground
-    if player.rect.collidepoint(playerPosX, playerPosY):
-        expl.drawGround(screen, playerPosX)
-        #freeze = 60
-        explode = True
-        print("ground collision")
-
-    if explode == True:
-        for i in enemys: 
-            if expl.rect.colliderect(i.rect):
-                print("hit")
-                num += 1
-                print(num)
-                enemys.remove(i)
-        freeze = 60
-        explode = False 
+        if explode == True:
+            for i in enemys: 
+                if expl.rect.colliderect(i.rect):
+                    print("hit")
+                    num += 1
+                    print(num)
+                    enemys.remove(i)
+            freeze = 60
+            explode = False 
 
 
 
-    
-    if freeze > 0:
-        freeze -= 1
-        expl.draw(screen)
+        
+        if freeze > 0:
+            freeze -= 1
+            expl.draw(screen)
 
-    player.update()
-    #print("enemy: " + str(enemy.rect.top));
+        player.update()
+        #print("enemy: " + str(enemy.rect.top));
 
-    for i in enemys:
-        i.move()
-        i.draw(screen)
+        for i in enemys:
+            i.move()
+            i.draw(screen)
 
-    player.draw(screen)
+        player.draw(screen)
 
-    
+        
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+        clock.tick(60)  # limits FPS to 60
 
+
+    #PLAY AS DINOSAUR
+    elif stage == 2:
+        # fill the screen with a color to wipe away anything from last frame
+
+        #screen.fill("red")
+        screen.blit(bg,(0, 0))
+        text = FONT.render(str(num), True, white)
+        textRect =  text.get_rect()
+        textRect.center = (400//2, 400//2)
+        screen.blit(text, textRect)
+
+
+        # RENDER GAME HERE
+        score = False
+        for i in enemys:
+            
+            if player.rect.colliderect(i.rect):
+                #player.image = pygame.image.load("./met1.PNG")
+                #player.image.get_rect()
+                explosion_x = i.rect.x
+                explosion_y = i.rect.y
+                player.rect.center=(-300, -300)
+                expl.rect.center=(explosion_x, 720);
+                #print("Player got Hit")
+                explode = True
+                #freeze = 60 
+
+            if i.rect.bottom >= 720:
+                playerPosX = i.rect.x
+                playerPosY = i.rect.y
+                #num += 1
+
+            #draw explosion object when hitting the ground
+            if i.rect.collidepoint(playerPosX, playerPosY):
+                #freeze = 60
+                
+                print("ground collision")
+                expl.drawGround(screen, playerPosX)
+                score = True
+
+        if explode == True:
+            score = False
+            expl.drawGround(screen, playerPosX)
+            print("Player got Hit")
+
+            player = ""
+            running = False
+            freeze = 60
+            explode = False 
+
+        if score == True:
+            num += 1
+
+
+
+        
+        if freeze > 0:
+            freeze -= 1
+            expl.draw(screen)
+
+        if player != "":
+            player.update()
+        #print("enemy: " + str(enemy.rect.top));
+
+        for i in enemys:
+            i.move()
+            i.draw(screen)
+
+        if player != "":
+            player.draw(screen)
+
+        
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+for i in range(60):
+    clock.tick(60)
 pygame.quit()
